ef1b5bea901b93b3fcb7c29a3de73eb3f1dd2d93
masukawa.kentaro <masukawa.kentaro@HL00881.local>
2022-01-31 18:48:11 +0900

    nodeアプリ作成

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-module.exports = function fizzbuzz (value) {
-  if (value % 15 === 0) {
+module.exports = function fizzbuzz (num) {
+  if (num % 15 === 0) {
     return 'FizzBuzz'
   }
-  if (value % 3 === 0) {
+  if (num % 3 === 0) {
     return 'Fizz'
   }
-  if (value % 5 === 0) {
+  if (num % 5 === 0) {
     return 'Buzz'
   }
-  return String(value)
+  return String(num)
 }
--- index.test.js
+++ index.test.js
@@ -1,8 +1,17 @@
 const fizzbuzz = require('./index')
 
-test('fizzbuzz', () => {
+test('returns FizzBuzz when num is divisible by both 3 and 5', () => {
   expect(fizzbuzz(30)).toBe('FizzBuzz')
-  expect(fizzbuzz(9)).toBe('Fizz')
+})
+
+test('returns Fizz when num is divisible by 3', () => {
+  expect(fizzbuzz(6)).toBe('Fizz')
+})
+
+test('returns Buzz when num is divisible by 5', () => {
   expect(fizzbuzz(10)).toBe('Buzz')
-  expect(fizzbuzz(7)).toBe('7')
+})
+
+test('returns the number when num is not divisible by 3 or 5', () => {
+  expect(fizzbuzz(4)).toBe('4')
 })
